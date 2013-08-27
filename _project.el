56f1e5f62c3bc9618ef942dcf87e84d44aa79be6
Philip Dorrell <philip@thinkinghard.com>
2013-08-28 09:35:30 +1200

    add :run-project-command to _project.el

--- _project.el
+++ _project.el
@@ -3,5 +3,7 @@
 (load-this-project
  `(
    (:search-extensions (".js" ".html" ".css" ".scss"))
+   (:run-project-command (javascript-run-file (project-file :main-javascript-file)))
+   (:main-javascript-file "test-bracketup.js")
    (:sass-watch-src-output-argument ,(concat (project-base-directory)))
     ) )
