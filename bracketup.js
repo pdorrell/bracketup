56d38f23a07d6e6a7a78709c9393db8e40d8be0a
Philip Dorrell <philip@thinkinghard.com>
2013-08-27 16:40:35 +1200

    pass tokens to testTokenReceiver

--- bracketup.js
+++ bracketup.js
@@ -2,22 +2,50 @@ function inspect(object) {
   return JSON.stringify(object);
 }
 
-var lexerRegex = /((\[)([A-Za-z0-9_\-,]*)([\s]*))|(\])|(\\.)|([^[\]\\]+)/g;
+var testTokenReceiver = {
+  startItem: function(itemArguments, whitespace) {
+    console.log("Start item " + inspect(itemArguments) + ", whitespace = " + inspect(whitespace));
+  }, 
+  endItem: function() {
+    console.log("End item");
+  }, 
+  text: function(string) {
+    console.log("Text: " + inspect(string));
+  }
+};
+
+var lexerRegex = /(?:(\[)([A-Za-z0-9_\-,]*)([\s]*))|(\])|(\\(.))|([^[\]\\]+)/g;
 
 var testLine = "[correspondence  [_title \\[Queens\\] Puzzle] " + 
   "[_text,rhoscript [_title rhoScript] [A [_word,1 8] [2 range]]";
 
-function scan(string, regex) {
+function scan(string, regex, tokenReceiver) {
   var match;
   var scanningRegex = new RegExp(regex.source, "g");
   console.log("Scanning " + inspect(string) + "\n  with " + scanningRegex + " ..."); 
-  var count = 0;
   var matchedSubstrings = [];
-  while ((match = scanningRegex.exec(string)) !== null && count < 100) {
-    console.log("  match = " + inspect(match));
-    console.log("  remainder = " + inspect(string.substr(scanningRegex.lastIndex)));
+  while ((match = scanningRegex.exec(string)) !== null) {
+    // console.log("  match = " + inspect(match));
     matchedSubstrings.push(match[0]);
-    count++;
+    // console.log("==> " + inspect(match[0]));
+    if(match[1]) {
+      var itemArguments = match[2].split(",");
+      var whitespace = match[3];
+      tokenReceiver.startItem(itemArguments, whitespace);
+    }
+    else if (match[4]) {
+      tokenReceiver.endItem();
+    }
+    else if (match[5]) {
+      tokenReceiver.text(match[6]);
+    }
+    else if (match[7]) {
+      tokenReceiver.text(match[7]);
+    }
+    else {
+      console.log("match = " + inspect(match));
+      throw new Error("No match found");
+    }
   }
   var reconstitutedMatches = matchedSubstrings.join("");
   if (reconstitutedMatches != string) {
@@ -30,7 +58,7 @@ function scan(string, regex) {
 var testRegex = /([A-Z]+)|([^A-Z]+)/g;
 var testLine2 = "ABCabcDEFghi";
 
-scan(testLine, lexerRegex);
+scan(testLine, lexerRegex, testTokenReceiver);
 
 
 
