08c3bafbeaf23ed4fb84033c275abd9d7e457672
Philip Dorrell <philip@thinkinghard.com>
2013-08-29 19:08:45 +1200

    assertions in correspondence-bracketup.js

--- test-correspondence-bracketup.js
+++ test-correspondence-bracketup.js
@@ -1,4 +1,5 @@
 var utils = require("./utils.js");
+var assert = require("assert");
 var merge = utils.merge;
 var inspect = utils.inspect;
 
@@ -6,7 +7,6 @@ var fs = require('fs');
 var correspondenceBracketup = require("./correspondence-bracketup.js");
 
 var testFileName = "sample.bracketup";
-
 var fileContents = fs.readFileSync(testFileName, {encoding: "utf-8"});
 
 var compiledObjects = correspondenceBracketup.compileCorrespondence(fileContents);
@@ -16,8 +16,14 @@ var jsdomDocument = jsdom(null, null, {});
 
 var document = new correspondenceBracketup.Document(jsdomDocument);
 
-for (var i=0; i<compiledObjects.length; i++) {
-  var correspondence = compiledObjects[i];
-  var correspondenceDom = correspondence.createDom(document);
-  console.log("correspondenceDom = \n\n" + correspondenceDom.outerHTML);
-}
+assert.equal(compiledObjects.length, 1);
+
+var expectedOutputFileName = "sample-bracket.output.html";
+var expectedOutput = fs.readFileSync(expectedOutputFileName, {encoding: "utf-8"});
+
+var correspondence = compiledObjects[0];
+var correspondenceDom = correspondence.createDom(document);
+var correspondenceDomHtml = correspondenceDom.outerHTML;
+
+assert.equal(expectedOutput, correspondenceDomHtml);
+
