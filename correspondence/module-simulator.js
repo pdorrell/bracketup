26b108ba64678fcbf77ceb804b86f95e8ff27c96
Philip Dorrell <philip@thinkinghard.com>
2013-08-29 12:25:53 +1200

    module-simulator: wrap in anon fun, and export as MODULE_SIMULATOR

--- correspondence/module-simulator.js
+++ correspondence/module-simulator.js
@@ -1,36 +1,40 @@
 /* A script to manage require/exports in Javascript files loaded by the browser, so that the
    same files can be synchronously loaded and executed in either Node or the browser */
 
-/** Javascript files can (and should) be wrapped in anonymous function calls, which use
-    "require" and "exports" as global variables. */
+/** Javascript files to be loaded can (and should) be wrapped in anonymous function calls, which use
+    "require" and "exports" as global variables, which this library manipulates directly. */
 
-/** exports as filled in by each module */
-exports={};
+var MODULE_SIMULATOR = window.MODULE_SIMULATOR || {};
 
-/** Accumulated list of loaded modules */
-requires={};
+(function() {
+  /** exports as filled in by each module */
+  exports={};  // create empty exports map ready for first script to be loaded
 
-/** Call this method after loading a specific Javascript file, to save it's
-    CommonJS exports as a module with a name of your choice.*/
-function saveExports(moduleName) {
-  requires[moduleName] = exports;
-  exports = {};
-}
+  /** Accumulated list of loaded modules */
+  var requires={};
 
-/** Call this method before loading a specific Javascript file, to specify
-    the resolution of all CommonJS "require" calls within the Javascript file.
-    Each resolution entry maps a URL to a named module (as saved by saveExports) */
-function setRequires(requireMap) {
-  require = function(url) {
-    var moduleName = requireMap[url];
-    if (!moduleName) {
-      throw new Error("Module for URL " + url + " not found");
-    }
-    var module = requires[moduleName];
-    if (!module) {
-      throw new Error("Module " + moduleName + " does not exist");
-    }
-    return module;
+  /** Call this method after loading a specific Javascript file, to save its
+      CommonJS exports as a module with a name of your choice.*/
+  MODULE_SIMULATOR.saveExports = function(moduleName) {
+    requires[moduleName] = exports;
+    exports = {}; // reset to empty exports map ready for next script to be loaded
   }
-}
 
+  /** Call this method before loading a specific Javascript file, to specify
+      the resolution of all CommonJS "require" calls within the Javascript file.
+      Each resolution entry maps a URL to a named module (as saved by saveExports) */
+  MODULE_SIMULATOR.setRequires = function(requireMap) {
+    require = function(url) {
+      var moduleName = requireMap[url]; // look up module name by URL
+      if (!moduleName) {
+        throw new Error("Module for URL " + url + " not found");
+      }
+      var module = requires[moduleName]; // look up module by module name
+      if (!module) {
+        throw new Error("Module " + moduleName + " does not exist");
+      }
+      return module;
+    }
+  }
+
+})();
