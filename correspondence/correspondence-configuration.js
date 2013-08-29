d033d7a8833aa03d70137d970605468c0f9d1051
Philip Dorrell <philip@thinkinghard.com>
2013-08-30 10:28:40 +1200

    error handling in web page configuration js

--- correspondence/correspondence-configuration.js
+++ correspondence/correspondence-configuration.js
@@ -11,18 +11,45 @@ var correspondenceBracketup = require("../correspondence-bracketup.js");
 
 var correspondenceCompiler = correspondenceBracketup.correspondenceCompiler;
 
+function nth(n){
+    if(isNaN(n) || n%1) return n;   
+    var s= n%100;
+    if(s>3 && s<21) return n+'th';
+    switch(s%10){
+        case 1: return n+'st';
+        case 2: return n+'nd';
+        case 3: return n+'rd';
+        default: return n+'th';
+    }
+}
+function ordinalSuffix(n) {
+  var nMod100 = n%100;
+  if(nMod100 > 3 && nMod100 < 21) {
+    return "th";
+  }
+  else {
+    var lastDigit = nMod100%10;
+    return lastDigit < 4 ? ["th", "st", "nd", "rd"][lastDigit] : "th";
+  }
+}
+
 function compileCorrespondenceSource(sourceElements) {
   sourceElements.each(function(index, sourceElement) {
     var sourceElementSelector = $(sourceElement);
     var correspondenceSource = sourceElementSelector.html();
     try {
-      var compiledDoms = correspondenceCompiler.compileDoms(correspondenceSource, document);
+      var scriptNumber = index+1;
+      var id = sourceElementSelector.attr("id");
+      var sourceFileName = scriptNumber + ordinalSuffix(scriptNumber) + 
+        " Correspondence script element" + 
+        (id ? " (id = " + id + ")" : "");
+      var compiledDoms = correspondenceCompiler.compileDoms(correspondenceSource, document, sourceFileName);
       for (var i=0; i<compiledDoms.length; i++) {
         sourceElementSelector.after(compiledDoms[i]);
       }
     }
     catch (error) {
-      alert("Error compiling Correspondence source: " + message + "\n" + 
+      alert("Error compiling Correspondence source: \"" + error.message + "\"\n\n" + 
             "See browser console for further details.");
       if (error.logSourceError) {
         error.logSourceError();
