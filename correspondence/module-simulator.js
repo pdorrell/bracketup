3fe764cbec6f39e02e6834c1f2dfb44ab9983821
Philip Dorrell <philip@thinkinghard.com>
2013-08-30 13:48:22 +1200

    relative script URL resolution

--- correspondence/module-simulator.js
+++ correspondence/module-simulator.js
@@ -7,6 +7,8 @@
 var MODULE_SIMULATOR = window.MODULE_SIMULATOR || {};
 
 (function() {
+  function inspect(object) {return JSON.stringify(object);}
+
   /** exports as filled in by each module */
   exports={};  // create empty exports map ready for first script to be loaded
 
@@ -14,6 +16,54 @@ var MODULE_SIMULATOR = window.MODULE_SIMULATOR || {};
   var requires={};
   
   var scriptUrl;
+  
+  function ScriptUrl(url) {
+    this.url = url;
+    this.urlParts = url.split("/");
+    this.pathParts = this.urlParts.slice(0, this.urlParts.length-1);
+  }
+  
+  ScriptUrl.prototype = {
+    toString: function() {
+      return this.url;
+    }, 
+    resolveRelatively: function (relativeUrl) {
+      var resolvedPath = [];
+      for (var i=0; i<this.pathParts.length; i++) {
+        var pathPart = this.pathParts[i];
+        if(pathPart != ".") {
+          resolvedPath.push(pathPart);
+        }
+      }
+      var relativeUrlParts = relativeUrl.split("/");
+      if (relativeUrlParts.length == 0) {
+        throw new Error("Relative script URL " + inspect(relativeUrl.url) + " has no path elements"); 
+      }
+      var firstPart = relativeUrlParts[0];
+      if (firstPart != "." && firstPart != "..") {
+        throw new Error("Relative script URL " + inspect(relativeUrl.url) + " does not start with '.' or '..'");
+      }
+      for (var i=0; i<relativeUrlParts.length; i++) {
+        var urlPart = relativeUrlParts[i];
+        if(urlPart == ".") {
+          // do nothing
+        }
+        else if (urlPart == "..") {
+          if (resolvedPath.length == 0) {
+            resolvedPath.push("..");
+          }
+          else {
+            resolvedPath.pop();
+          }
+        }
+        else {
+          resolvedPath.push(urlPart);
+        }
+      }
+      var resolvedPathParts = resolvedPath[0] == ".." ? resolvedPath : ["."].concat(resolvedPath);
+      return resolvedPathParts.join("/");
+    }
+  };
 
   /** Call this method after loading a specific Javascript file, to save its
       CommonJS exports as a module with a name of your choice.*/
@@ -27,14 +77,17 @@ var MODULE_SIMULATOR = window.MODULE_SIMULATOR || {};
       Each resolution entry maps a URL to a named module (as saved by saveExports) */
   function setRequires(requireMap) {
     require = function(url) {
-      console.log("Requiring " + url + " ...");
+      console.log("  requiring " + url + " ...");
+      var urlResolvedRelatively = scriptUrl.resolveRelatively(url);
       var moduleName = requireMap[url]; // look up module name by URL
-      console.log(" moduleName = " + moduleName);
+      if (moduleName != urlResolvedRelatively) {
+        throw new Error("Inconsistent relative URL resolution: " + inspect(urlResolvedRelatively) + 
+                       " != " + inspect(moduleName));
+      }
       if (!moduleName) {
         throw new Error("Module for URL " + url + " not found");
       }
       var module = requires[moduleName]; // look up module by module name
-      console.log("  module = " + module);
       if (!module) {
         throw new Error("Module " + moduleName + " does not exist");
       }
@@ -45,11 +98,12 @@ var MODULE_SIMULATOR = window.MODULE_SIMULATOR || {};
   MODULE_SIMULATOR.startScript = function(url, requireMap) {
     console.log("MODULE_SIMULATOR.startScript " + url);
     setRequires(requireMap ? requireMap : {});
-    scriptUrl = url;
+    scriptUrl = new ScriptUrl(url);
   }
   
   MODULE_SIMULATOR.endScript = function() {
-    saveExports(scriptUrl);
+    console.log("MODULE_SIMULATOR.endScript " + scriptUrl.url);
+    saveExports(scriptUrl.url);
     scriptUrl = null;
   }
 
