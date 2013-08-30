65671e06db52469874d92e7a00b629ef4a6f1885
Philip Dorrell <philip@thinkinghard.com>
2013-08-30 13:25:57 +1200

    MODULE_SIMULATOR startScript & endScript

--- correspondence/module-simulator.js
+++ correspondence/module-simulator.js
@@ -12,10 +12,12 @@ var MODULE_SIMULATOR = window.MODULE_SIMULATOR || {};
 
   /** Accumulated list of loaded modules */
   var requires={};
+  
+  var scriptUrl;
 
   /** Call this method after loading a specific Javascript file, to save its
       CommonJS exports as a module with a name of your choice.*/
-  MODULE_SIMULATOR.saveExports = function(moduleName) {
+  function saveExports(moduleName) {
     requires[moduleName] = exports;
     exports = {}; // reset to empty exports map ready for next script to be loaded
   }
@@ -23,18 +25,32 @@ var MODULE_SIMULATOR = window.MODULE_SIMULATOR || {};
   /** Call this method before loading a specific Javascript file, to specify
       the resolution of all CommonJS "require" calls within the Javascript file.
       Each resolution entry maps a URL to a named module (as saved by saveExports) */
-  MODULE_SIMULATOR.setRequires = function(requireMap) {
+  function setRequires(requireMap) {
     require = function(url) {
+      console.log("Requiring " + url + " ...");
       var moduleName = requireMap[url]; // look up module name by URL
+      console.log(" moduleName = " + moduleName);
       if (!moduleName) {
         throw new Error("Module for URL " + url + " not found");
       }
       var module = requires[moduleName]; // look up module by module name
+      console.log("  module = " + module);
       if (!module) {
         throw new Error("Module " + moduleName + " does not exist");
       }
       return module;
     }
+  }
+  
+  MODULE_SIMULATOR.startScript = function(url, requireMap) {
+    console.log("MODULE_SIMULATOR.startScript " + url);
+    setRequires(requireMap ? requireMap : {});
+    scriptUrl = url;
+  }
+  
+  MODULE_SIMULATOR.endScript = function() {
+    saveExports(scriptUrl);
+    scriptUrl = null;
   }
 
 })();
