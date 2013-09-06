6634ee2674fde7e521a76a43d163917ef33644f6
Philip Dorrell <philip@thinkinghard.com>
2013-09-06 19:57:31 +1200

    include .coffee files in text search

--- _project.el
+++ _project.el
@@ -2,7 +2,7 @@
 
 (load-this-project
  `(
-   (:search-extensions (".js" ".html" ".css" ".scss"))
+   (:search-extensions (".js" ".html" ".css" ".scss" ".coffee"))
    (:run-project-command (javascript-run-file (project-file :main-javascript-file)))
    ;;(:main-javascript-file "test-correspondence-bracketup.js")
    (:main-javascript-file "compile-and-run.js")
