e6ba178e6d14f06e9895cbcfbe43c517a84ed685
Philip Dorrell <philip@thinkinghard.com>
2013-09-06 20:07:45 +1200

    remove anon function wrapper from correspondence-bracketup.js

--- correspondence-bracketup.js
+++ correspondence-bracketup.js
@@ -1,107 +1,105 @@
-(function() {
-  var utils = require("./utils.js");
-  var merge = utils.merge;
-  var inspect = utils.inspect;
+var utils = require("./utils.js");
+var merge = utils.merge;
+var inspect = utils.inspect;
 
-  var bracketup = require("./bracketup.js");
+var bracketup = require("./bracketup.js");
 
-  function TitleAttribute() {
-    bracketup.BaseAttribute.call(this, "title");
-  }
+function TitleAttribute() {
+  bracketup.BaseAttribute.call(this, "title");
+}
 
-  TitleAttribute.prototype = merge(bracketup.BaseAttribute.prototype, {
-  });
+TitleAttribute.prototype = merge(bracketup.BaseAttribute.prototype, {
+});
 
-  function LanguageTitleAttribute() {
-    bracketup.BaseAttribute.call(this, "languageTitle");
-  }
+function LanguageTitleAttribute() {
+  bracketup.BaseAttribute.call(this, "languageTitle");
+}
 
-  LanguageTitleAttribute.prototype = merge(bracketup.BaseAttribute.prototype, {
-  });
+LanguageTitleAttribute.prototype = merge(bracketup.BaseAttribute.prototype, {
+});
 
-  function Word(id) {
-    bracketup.BaseNode.call(this);
-    this.id = id;
-  }
+function Word(id) {
+  bracketup.BaseNode.call(this);
+  this.id = id;
+}
 
-  Word.prototype = merge(bracketup.BaseNode.prototype, {
-    createInitialDom: function(document) {
-      var id = this.id;
-      if (id.match(/^[0-9]+$/)) {
-        id = this.parent.id + id;
-      }
-      return document.createNode("span", {className: "item", 
-                                          attributes: {"data-id": id}});
+Word.prototype = merge(bracketup.BaseNode.prototype, {
+  createInitialDom: function(document) {
+    var id = this.id;
+    if (id.match(/^[0-9]+$/)) {
+      id = this.parent.id + id;
     }
-  });
-
-  function Sentence(id) {
-    bracketup.BaseNode.call(this);
-    this.id = id;
+    return document.createNode("span", {className: "item", 
+                                        attributes: {"data-id": id}});
   }
+});
 
-  Sentence.prototype = merge(bracketup.BaseNode.prototype, {
-    defaultChildFunction: "word", 
-    classMap: {word: Word}, 
-    childIndent: "  ", 
-    
-    createInitialDom: function(document) {
-      return document.createNode("div", {className: "item-group", 
-                                         attributes: {"data-group-id": this.id}});
-    }
-  });
+function Sentence(id) {
+  bracketup.BaseNode.call(this);
+  this.id = id;
+}
 
-  function Text(languageCssClass) {
-    bracketup.BaseNode.call(this);
-    this.languageCssClass = languageCssClass;
-  }
-
-  Text.prototype = merge(bracketup.BaseNode.prototype, {
-    defaultChildFunction: "sentence", 
-    classMap: {sentence: Sentence, languageTitle: LanguageTitleAttribute}, 
-    childIndent: "  ", 
-    indentAllChildren: true, 
-    ignoreWhiteSpaceText: true, 
-    
-    createInitialDom: function(document) {
-      var className = this.languageCssClass ? ("structure " + this.languageCssClass + "-structure") : "structure";
-      var div = document.createNode("div", {className: className});
-      var languageTitle = this.attributes.languageTitle;
-      if (languageTitle) {
-        document.createNode("div", {parent: div, className: "language", 
-                                    text: languageTitle});
-      }
-      return div;
-    }
-  });
-
-  function Correspondence() {
-    bracketup.BaseNode.call(this);
-  }
-
-  Correspondence.prototype = merge(bracketup.BaseNode.prototype, {
-    defaultChildFunction: "text", 
-    classMap: {text: Text, title: TitleAttribute}, 
-    childIndent: "  ", 
-    indentAllChildren: true, 
-    ignoreWhiteSpaceText: true, 
-    
-    createInitialDom: function (document) {
-      var div = document.createNode("div", {className: "structure-group"});
-      var title = this.attributes.title;
-      if (title) {
-        document.createNode("div", {parent: div, className: "description", 
-                                    text: title});
-      }
-      return div;
-    }
-  });
+Sentence.prototype = merge(bracketup.BaseNode.prototype, {
+  defaultChildFunction: "word", 
+  classMap: {word: Word}, 
+  childIndent: "  ", 
   
-  var correspondenceCompiler = 
-    new bracketup.BracketupCompiler({correspondence: Correspondence, 
-                                     b: bracketup.Bold, 
-                                     i: bracketup.Italic, 
-                                     a: bracketup.Link});
+  createInitialDom: function(document) {
+    return document.createNode("div", {className: "item-group", 
+                                       attributes: {"data-group-id": this.id}});
+  }
+});
 
-  exports.correspondenceCompiler = correspondenceCompiler;
-})();
+function Text(languageCssClass) {
+  bracketup.BaseNode.call(this);
+  this.languageCssClass = languageCssClass;
+}
+
+Text.prototype = merge(bracketup.BaseNode.prototype, {
+  defaultChildFunction: "sentence", 
+  classMap: {sentence: Sentence, languageTitle: LanguageTitleAttribute}, 
+  childIndent: "  ", 
+  indentAllChildren: true, 
+  ignoreWhiteSpaceText: true, 
+  
+  createInitialDom: function(document) {
+    var className = this.languageCssClass ? ("structure " + this.languageCssClass + "-structure") : "structure";
+    var div = document.createNode("div", {className: className});
+    var languageTitle = this.attributes.languageTitle;
+    if (languageTitle) {
+      document.createNode("div", {parent: div, className: "language", 
+                                  text: languageTitle});
+    }
+    return div;
+  }
+});
+
+function Correspondence() {
+  bracketup.BaseNode.call(this);
+}
+
+Correspondence.prototype = merge(bracketup.BaseNode.prototype, {
+  defaultChildFunction: "text", 
+  classMap: {text: Text, title: TitleAttribute}, 
+  childIndent: "  ", 
+  indentAllChildren: true, 
+  ignoreWhiteSpaceText: true, 
+  
+  createInitialDom: function (document) {
+    var div = document.createNode("div", {className: "structure-group"});
+    var title = this.attributes.title;
+    if (title) {
+      document.createNode("div", {parent: div, className: "description", 
+                                  text: title});
+    }
+    return div;
+  }
+});
+
+var correspondenceCompiler = 
+  new bracketup.BracketupCompiler({correspondence: Correspondence, 
+                                   b: bracketup.Bold, 
+                                   i: bracketup.Italic, 
+                                   a: bracketup.Link});
+
+exports.correspondenceCompiler = correspondenceCompiler;
